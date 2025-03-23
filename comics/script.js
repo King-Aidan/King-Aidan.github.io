6c7fc924607ab6f5eef37ca4248773a07175989a
Pikmin-bleu <charmilles120@gmail.com>
2025-03-23 17:31:16 -0500

    jkl
    hj

---
+++ comics/script.js
@@ -0,0 +1,130 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+function enlargeviolence()
+{
+  var here = new String();
+  here = document.getElementById("violence").style.display;
+
+  if(here == "none" || here == "")
+  {
+      document.getElementById("violence").style.display = "inherit";
+  }
+  else
+  {
+      document.getElementById("violence").style.display = "none";
+  }
+}
+
+function enlargestorm()
+{
+  var here = new String();
+  here = document.getElementById("storm").style.display;
+
+  if(here == "none" || here == "")
+  {
+      document.getElementById("storm").style.display = "inherit";
+  }
+  else
+  {
+      document.getElementById("storm").style.display = "none";
+  }
+}
+
+function enlargeharlege1()
+{
+  var here = new String();
+  here = document.getElementById("harlege1").style.display;
+
+  if(here == "none" || here == "")
+  {
+      document.getElementById("harlege1").style.display = "inherit";
+  }
+  else
+  {
+      document.getElementById("harlege1").style.display = "none";
+  }
+}
+
+function enlargeharlege2()
+{
+  var here = new String();
+  here = document.getElementById("harlege2").style.display;
+
+  if(here == "none" || here == "")
+  {
+      document.getElementById("harlege2").style.display = "inherit";
+  }
+  else
+  {
+      document.getElementById("harlege2").style.display = "none";
+  }
+}
+
+function enlargeyou()
+{
+  var here = new String();
+  here = document.getElementById("you").style.display;
+
+  if(here == "none" || here == "")
+  {
+      document.getElementById("you").style.display = "inherit";
+  }
+  else
+  {
+      document.getElementById("you").style.display = "none";
+  }
+}
+
+function enlargesnack1()
+{
+  var here = new String();
+  here = document.getElementById("snack1").style.display;
+
+  if(here == "none" || here == "")
+  {
+      document.getElementById("snack1").style.display = "inherit";
+  }
+  else
+  {
+      document.getElementById("snack1").style.display = "none";
+  }
+}
+
+function enlargesnack2()
+{
+  var here = new String();
+  here = document.getElementById("snack2").style.display;
+
+  if(here == "none" || here == "")
+  {
+      document.getElementById("snack2").style.display = "inherit";
+  }
+  else
+  {
+      document.getElementById("snack2").style.display = "none";
+  }
+}
+
+function enlargeinterest()
+{
+  var here = new String();
+  here = document.getElementById("interest").style.display;
+
+  if(here == "none" || here == "")
+  {
+      document.getElementById("interest").style.display = "inherit";
+  }
+  else
+  {
+      document.getElementById("interest").style.display = "none";
+  }
+}
+
+
