5f05946040caaf5f03c1ccc45418ee3f39d1578a
Pikmin-bleu <charmilles120@gmail.com>
2025-03-23 22:25:55 -0500

    j
    hj

---
+++ illustration/script.js
@@ -0,0 +1,162 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+  function enlargecape()
+  {
+    var here = new String();
+    here = document.getElementById("cape").style.display;
+  
+    if(here == "none" || here == "")
+    {
+        document.getElementById("cape").style.display = "inherit";
+    }
+    else
+    {
+        document.getElementById("cape").style.display = "none";
+    }
+  }
+  
+  function enlargealpha()
+  {
+    var here = new String();
+    here = document.getElementById("alpha").style.display;
+  
+    if(here == "none" || here == "")
+    {
+        document.getElementById("alpha").style.display = "inherit";
+    }
+    else
+    {
+        document.getElementById("alpha").style.display = "none";
+    }
+  }
+  
+  function enlargedocks()
+  {
+    var here = new String();
+    here = document.getElementById("docks").style.display;
+  
+    if(here == "none" || here == "")
+    {
+        document.getElementById("docks").style.display = "inherit";
+    }
+    else
+    {
+        document.getElementById("docks").style.display = "none";
+    }
+  }
+  
+  function enlargefunny()
+  {
+    var here = new String();
+    here = document.getElementById("funny").style.display;
+  
+    if(here == "none" || here == "")
+    {
+        document.getElementById("funny").style.display = "inherit";
+    }
+    else
+    {
+        document.getElementById("funny").style.display = "none";
+    }
+  }
+  
+  function enlargelesbians()
+  {
+    var here = new String();
+    here = document.getElementById("lesbians").style.display;
+  
+    if(here == "none" || here == "")
+    {
+        document.getElementById("lesbians").style.display = "inherit";
+    }
+    else
+    {
+        document.getElementById("lesbians").style.display = "none";
+    }
+  }
+  
+  function enlargemessy()
+  {
+    var here = new String();
+    here = document.getElementById("messy").style.display;
+  
+    if(here == "none" || here == "")
+    {
+        document.getElementById("messy").style.display = "inherit";
+    }
+    else
+    {
+        document.getElementById("messy").style.display = "none";
+    }
+  }
+  
+  function enlargepinup()
+  {
+    var here = new String();
+    here = document.getElementById("pinup").style.display;
+  
+    if(here == "none" || here == "")
+    {
+        document.getElementById("pinup").style.display = "inherit";
+    }
+    else
+    {
+        document.getElementById("pinup").style.display = "none";
+    }
+  }
+  
+  function enlargezach()
+  {
+    var here = new String();
+    here = document.getElementById("zach").style.display;
+  
+    if(here == "none" || here == "")
+    {
+        document.getElementById("zach").style.display = "inherit";
+    }
+    else
+    {
+        document.getElementById("zach").style.display = "none";
+    }
+  }
+
+  function enlargeemoji()
+  {
+    var here = new String();
+    here = document.getElementById("emoji").style.display;
+  
+    if(here == "none" || here == "")
+    {
+        document.getElementById("emoji").style.display = "inherit";
+    }
+    else
+    {
+        document.getElementById("emoji").style.display = "none";
+    }
+  }
+  
+  function enlargevoices()
+  {
+    var here = new String();
+    here = document.getElementById("voices").style.display;
+  
+    if(here == "none" || here == "")
+    {
+        document.getElementById("voices").style.display = "inherit";
+    }
+    else
+    {
+        document.getElementById("voices").style.display = "none";
+    }
+  }
+  
+  
+  
